fab057a98e2fad6ff7c95b8359ed5071a25e3bfb
Delos Chang <lol.i.laugh@gmail.com>
2013-02-01 20:42:28 -0500

    successfully scraped one item in the nutrition menu. need to expand to entire menu

--- nutrition_scraper.py
+++ nutrition_scraper.py
@@ -6,13 +6,16 @@ import json
 #soup = BeautifulSoup(urllib2.urlopen('http://nutrition.dartmouth.edu:8088/cwp?nocache=1359761631598').read())
 
 url = 'http://nutrition.dartmouth.edu:8088/cwp'
-parameters = {'nocache':1359761609459}
+#parameters = {'id':8, 'method':'get_nutrient_label_items', 'param':'[{"0":"sid"},{"1":}
+
+#parameteres = s
+parameters = {"service":"","method":"get_nutrient_label_items","id":8,"params":[{"sid":"DDS.8a220b5994c384ca0d2935ac172c668e"},"{\"remoteProcedure\":\"get_nutrient_label_items\",\"mm_id\":9328,\"recipe_id\":-5272,\"mmr_rank\":13,\"rule\":\"fda|raw\",\"output\":\"dictionary\",\"options\":\"facts\",\"cache\":true,\"recdata\":null}"]}
 data = json.dumps(parameters)
 
 headers = {"Content-Type": "application/json",
 'Content-Length' : len(data),
 "Referer":"http://nutrition.dartmouth.edu:8088/",
-"Cookie":'JSESSIONID=2C6BBA00328C1C2F67794E50337D6E3A.N1TS002',
+#"Cookie":'JSESSIONID=2C6BBA00328C1C2F67794E50337D6E3A.N1TS002',
 "User-Agent":'Mozilla/5.0 (X11; Linux x86_64; rv:10.0.1) Gecko/20100101 Firefox/10.0.1',
 "method":"get_nutrient_label_items",
 "params": "",
